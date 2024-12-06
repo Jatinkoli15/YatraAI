619ab0092539ed354a348f919e3496ebb547d0c0
Jatin <jatinkoli65@gmail.com>
2024-12-06 12:19:58 +0530

    ban-gaya-project

---
+++ src/components/custom/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from '../ui/button';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  const openInstagram = () => {
+    window.open('https://github.com/satendra03', '_blank', 'noopener,noreferrer');
+  };
+  return (
+    <div className="footer min-h-32 w-full flex text-muted-foreground items-center justify-center p-4">
+     
+    </div>
+  );
+}
+
+export default Footer;
